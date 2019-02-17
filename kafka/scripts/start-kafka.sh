e94f801b665e116727d6f0924580fecb1e250383
Tomasz Sarnowski <sarnowskitomek@gmail.com>
2019-02-17 17:46:24 +0100

    Fix run issue

--- kafka/scripts/start-kafka.sh
+++ kafka/scripts/start-kafka.sh
@@ -14,6 +14,9 @@ if [ ! -z "$HELIOS_PORT_kafka" ]; then
     ADVERTISED_PORT=`echo $HELIOS_PORT_kafka | cut -d':' -f 2`
 fi
 
+echo "" >> $KAFKA_HOME/config/server.properties
+
+
 # Set the external host and port
 if [ ! -z "$ADVERTISED_HOST" ]; then
     echo "advertised host: $ADVERTISED_HOST"
